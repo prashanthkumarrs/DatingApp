37b0908e4ac7a4e22636ec98d274987b73d7240e
Prashanth RS <prashanthratala@gmail.com>
2021-02-23 22:59:18 +0530

    End of Section 8

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,37 +1,40 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
-using API.Data;
-using API.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.EntityFrameworkCore;
+using API.Interfaces;
+using API.DTOs;
+using AutoMapper;
 
 namespace API.Controllers
 {
+    [Authorize]
     public class UsersController : BaseApiController
     {
-        private readonly DataContext _context;
-        public UsersController(DataContext context)
+        private readonly IUserRepository _userRepository;
+        private readonly IMapper _mapper;
+
+        public UsersController(IUserRepository userRepository, IMapper mapper)
         {
-            _context = context;
+            _mapper = mapper;
+            _userRepository = userRepository;
         }
 
         [HttpGet]
-        [AllowAnonymous]
-        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers()
         {
-            return await _context.Users.ToListAsync();
+          var users = await _userRepository.GetMembersAsync();
+
+           return Ok(users);
         }
-           
-        [Authorize]   
-           
 
         //api/users/3
-         [HttpGet("{id}")]
+        [HttpGet("{username}")]
 
-        public async Task<ActionResult<AppUser>> GetUser(int id)
+        public async Task<ActionResult<MemberDto>> GetUser(string username)
         {
-            return await _context.Users.FindAsync(id);
+            return await _userRepository.GetMemberAsync(username); 
         }
     }
 }
---
+++ API/Helpers/AutoMapperProfiles.cs
@@ -0,0 +1,20 @@
+using AutoMapper;
+using API.Entities;
+using API.DTOs;
+using System.Linq;
+using API.Extensions;
+
+namespace API.Helpers
+{
+    public class AutoMapperProfiles : Profile
+    {
+           public AutoMapperProfiles()
+        {
+            CreateMap<AppUser, MemberDto>()
+            .ForMember(dest => dest.PhotoUrl, opt=> opt.MapFrom
+            (sourceMember=>sourceMember.Photos.FirstOrDefault(x=>x.IsMain).Url))
+            .ForMember(dest=> dest.Age, opt =>opt.MapFrom(src => src.DateofBirth.CalculateAge()));
+            CreateMap<Photo, PhotoDto>();
+        }
+    }
+}
